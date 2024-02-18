6b8668e4362932e64139ab05e19824f66af9c796
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-02-18 13:54:56 -0300

    finalizado edição home

--- src/components/midleHome/index.jsx
+++ src/components/midleHome/index.jsx
@@ -1,14 +1,18 @@
 "use client";
 import Image from 'next/image'
-
 import ImgHome from '../../img/imgHome.webp';
+import Mask from '../../img/icon/Mask.svg';
+import Desc from '../../img/icon/Description.svg';
+import Eng from '../../img/icon/Engineering.svg';
+import Chek from '../../img/icon/Checklist.svg';
+
 
 import style from './style.css';
 const MidleHome = () => {
     return (
         <div className='MidleHome'>
-            <hr className='Hr'/>
-            <Image className='MidleHome_img' src={ImgHome} alt='Foto Logo'/>
+            <hr className='Hr' />
+            <Image className='MidleHome_img' src={ImgHome} alt='Foto Logo' />
             <p>
                 Nós acreditamos que a tecnologia deve estar sempre voltada para melhorar a vida das pessoas, começando pela saúde.
 
@@ -16,7 +20,27 @@ const MidleHome = () => {
 
                 Possuímos ampla experiência, não apenas em manutenção de equipamentos médicos, mas também na diversificação de soluções em serviços na área clínica.
 
-                Nossa área de atuação é: Engenharia Clínica; Gestão de Manutenção; Especificação e Aquisição de Equipamentos Biomedicos e Consultoria em Processos de Aquisição de Equipamentos (SICONV).
+                <br />
+                Nossa área de atuação é: <br />
+                <ul className='Home_list'>
+                    <li className='Home_list_item'>
+                        <Image className='icon_img' src={Mask} alt='Foto Logo' />
+                        <h4>Engenharia Clínica;</h4>
+                    </li>
+                    <li className='Home_list_item'>
+                        <Image className='icon_img' src={Eng} alt='Foto Logo' />
+                        <h4>Gestão de Manutenção;</h4>
+                    </li>
+                    <li className='Home_list_item'>
+                        <Image className='icon_img' src={Chek} alt='Foto Logo' />
+                        <h4>Especificação e Aquisição de Equipamentos Biomedicos;</h4>
+                    </li>
+                    <li className='Home_list_item'>
+                        <Image className='icon_img' src={Desc} alt='Foto Logo' />
+                        <h4>Consultoria em Processos de Aquisição de Equipamentos (SICONV);</h4>
+                    </li>
+                </ul>
+
             </p>
         </div>
     )
