d7135c29c6496a96807781ec0dbd7b0cb530205a
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-02-18 14:36:56 -0300

    finalizado style services

--- src/components/MidleService/index.jsx
+++ src/components/MidleService/index.jsx
@@ -3,53 +3,53 @@ import Image from 'next/image'
 import ImgHService from '../../img/negocio .webp';
 import style from './style.css'
 
+import Mask from '../../img/icon/Mask.svg';
+import Desc from '../../img/icon/Description.svg';
+import Eng from '../../img/icon/Engineering.svg';
+import Chek from '../../img/icon/Checklist.svg';
+
 const MidleService = () => {
     return (
         <div className='MidleService'>
             <hr className='Hr' />
-            <Image className='MidleHome_Service_img' src={ImgHService} alt='Foto Logo' />
-            <p className='p_service'>
-
-                Serviços:
-
-                Mapeamento de parque tecnológico
-                Inventario com as informações principais dos equipamentos como marca, modelo, número de série, imobilizado, local de instalação, ano de fabricação
-
-
-                Realização de cronograma para Manutenções Preventivas (MP) e Calibrações;
-
-
-
-                Levantamento de partes e peças para realização de MP;
-
-
-
-                Auxílio na estruturação tecnológica - buscando através do levantamento atual do parque as prioridades e necessidades de substituição, bem como implementação de novas tecnologias;
-
-
-
-                Realização de Manutenção Preventiva e Manutenção Corretiva;
-
-
-
-                Implementação de serviço de Engenharia Clínica;
-
-
-
-                Consultoria em processos de aquisição de dispositivos médicos - através de verbas parlamentares, utilizando sistemas como SICONV;
-
-
-
-                Suporte técnico na especificação de partes e peças;
-
-
-
-                Levantamento de necessidades de treinamentos de equipes quanto a utilização adequada dos equipamentos;
-
-
-
-                Treinamentos a equipes quanto a utilização de equipamentos.
-            </p>
+            <Image className='MidleService_Service_img' src={ImgHService} alt='Foto Logo' />
+            <h2 className='p_service'>
+                Serviços
+            </h2>
+            <ul className='Service_list'>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Mask} alt='Foto Logo' />
+                        <h4>Realização de Manutenção Preventiva e Manutenção Corretiva;</h4>
+                    </li>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Eng} alt='Foto Logo' />
+                        <h4>Suporte técnico na especificação de partes e peças;</h4>
+                    </li>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Chek} alt='Foto Logo' />
+                        <h4>Realização de cronograma para Manutenções Preventivas (MP) e Calibrações;</h4>
+                    </li>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Desc} alt='Foto Logo' />
+                        <h4>Treinamentos a equipes quanto a utilização de equipamentos;</h4>
+                    </li>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Desc} alt='Foto Logo' />
+                        <h4>Auxílio na estruturação tecnológica - buscando através do levantamento atual do parque as prioridades e necessidades de substituição, bem como implementação de novas tecnologias;</h4>
+                    </li>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Desc} alt='Foto Logo' />
+                        <h4>Mapeamento de parque tecnológico Inventario com as informações principais dos equipamentos como marca, modelo, número de série, imobilizado, local de instalação, ano de fabricação;</h4>
+                    </li>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Desc} alt='Foto Logo' />
+                        <h4>Levantamento de necessidades de treinamentos de equipes quanto a utilização adequada dos equipamentos;</h4>
+                    </li>
+                    <li className='Service_list_item'>
+                        <Image className='icon_img' src={Desc} alt='Foto Logo' />
+                        <h4>Implementação de serviço de Engenharia Clínica; Consultoria em processos de aquisição de dispositivos médicos - através de verbas parlamentares, utilizando sistemas como SICONV; </h4>
+                    </li>
+                </ul>
         </div>
     )
 
