c691162cd15487bf64460f17571ad341539f562e
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-02-18 18:56:34 -0300

    finalizado abertura de chamado

---
+++ src/app/chamado/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Header from "@/components/header";
+import Btn from "@/components/buttons/bt.jsx";
+import MidleChamado from "@/components/MidleCall/index.jsx";
+
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <Btn/>
+      <MidleChamado/>
+    </>
+  );
+}
---
+++ src/components/MidleCall/index.jsx
@@ -0,0 +1,16 @@
+'use client';
+import ContactFormCall from "../contact-form-call.js";
+import style from './style.css'
+
+const MidleCall = () => {
+    return (
+        <div className='MidleService'>
+            <hr className='Hr' />
+            <ContactFormCall />
+        </div>
+    )
+
+}
+
+
+export default MidleCall;
